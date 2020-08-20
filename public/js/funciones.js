1b8fb1efb793eaca3a939d33dfca23b8bd4ca09e
OSCAR EDUARDO <oscareduardorodriguezcastro3@gmail.com>
2020-08-20 09:40:28 -0500

    vistas

--- public/js/funciones.js
+++ public/js/funciones.js
@@ -20,16 +20,19 @@ var txcontraseñas = document.getElementById('contraseñas');
 var txcorreos = document.getElementById('correos');
 var listausuarios = document.getElementById('listausuarios');
 var btnagregar = document.getElementById('btnagregar');
+var btnleer = document.getElementById('btnleer');
 
 // funciones
 
 
-
+// function abrirpaquetes(){
+//     location.href = "paquetes.html";
+// }
 
 function agregardatos() {
     leerDatos();
     db.collection("usuarios").add({
-        Nombre: txnombres.value,
+        nombre: txnombres.value,
         correo: txcorreos.value,
         contra: txcontraseñas.value,
     })
@@ -56,8 +59,8 @@ function leerDatos() {
                        <td>${doc.data().correo}</td>
                        <td>${doc.data().contra}</td>
                        <td>
-                           <button onclick="eliminar('${doc.id}')" class="btn btn-danger"><i class="far fa-trash-alt"></i></button>
-                           <button onclick="editar('${doc.id}')" class="btn btn-info"><i class="far fa-edit"></i></button>
+                           <button onclick="eliminar('${doc.id}')" class="btn btn-danger"><i class="far fa-trash-alt"></i>delete</button>
+                           <button onclick="editar('${doc.id}')" class="btn btn-info"><i class="far fa-edit"></i>edit</button>
                        </td>
                    </tr>           
                `;
@@ -66,4 +69,52 @@ function leerDatos() {
         .catch(function (error) {
             console.log("Error: ", error);
         });
+}
+
+function eliminar(id) {
+    db.collection("usuarios").doc(id).delete()
+        .then(() => {
+            console.log("Documento eliminado");
+            leerDatos();
+        }).catch((error) => {
+            console.error("Error: ", error);
+        });
+}
+
+function editar(id) {
+    btnAgregar.classList.add('d-none');
+    btnActualizar.classList.remove('d-none');
+    db.collection("usuarios").doc(id).get()
+        .then((doc) => {
+            idUsuario.value = id;
+            txtname.value = doc.data().nombre;
+            apellidos.value = doc.data().apellido;
+        })
+        .catch((error) => {
+            console.log("Error: ", error);
+        });
+}
+
+function actualizarDatos() {
+    db.collection("usuarios").doc(idUsuario.value).update({
+        nombre: txtname.value,
+        apellido: apellidos.value,
+        rol: opcion.value
+    })
+        .then(() => {
+            limpiarDatos()
+            leerDatos();
+            btnActualizar.classList.add('d-none');
+            btnAgregar.classList.remove('d-none');
+            console.log("Document successfully updated!");
+        })
+        .catch((error) => {
+            console.log("Error: ", error);
+        });
+
+}
+
+function limpiarDatos() {
+    txtname.value = "";
+    apellidos.value = "";
 }
--- public/js/index.js
+++ public/js/index.js
@@ -7,13 +7,13 @@
    console.log(usuario.value + " " + contraseña.value);
 
    if (usuario.value == "admin" && contraseña.value == "admin") {
-     location.href = "panel.html";
+     location.href = "admin.html";
      alert("inicaste")
      var inf2 = document.getElementById('info2');
      inf2.style.display = none;
    } else {
      alert("estas equivocado")
-     location.href = "panel.html"
+     location.href = "admin.html"
      var info1 = document.getElementById('info1');
      info1.style.display = none;
 
